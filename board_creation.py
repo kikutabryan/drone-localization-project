dbccdd8547e672f88512e1d342a387f3fef24709
kikutabryan <kikutabryan@gmail.com>
2023-05-23 17:40:00 -0400

    Added connection to drone and code cleaning

--- board_creation.py
+++ board_creation.py
@@ -1,12 +1,12 @@
 import cv2
 from cv2 import aruco
 
-def create_aruco_marker_board(markers_X, markers_Y, marker_length, marker_separation, output_file):
+def create_aruco_marker_board(markers_x, markers_y, marker_length, marker_separation, output_file):
     # Define the parameters for the marker board
-    board = aruco.GridBoard_create(markers_X, markers_Y, marker_length, marker_separation, aruco.getPredefinedDictionary(aruco.DICT_6X6_250))
+    board = aruco.GridBoard_create(markers_x, markers_y, marker_length, marker_separation, aruco.getPredefinedDictionary(aruco.DICT_6X6_250))
 
     # Generate the marker board image
-    size = (marker_length + marker_separation) * markers_X
+    size = (marker_length + marker_separation) * markers_x
     board_image = board.draw((size, size))
 
     # Save the marker board image to a file
@@ -15,11 +15,11 @@ def create_aruco_marker_board(markers_X, markers_Y, marker_length, marker_separa
     print(f"Marker board image saved as '{output_file}'")
 
 # Define the parameters for the marker board
-markers_X = 4  # Number of markers along the X-axis
-markers_Y = 4  # Number of markers along the Y-axis
+markers_x = 4  # Number of markers along the X-axis
+markers_y = 4  # Number of markers along the Y-axis
 marker_length = 100  # Length of each marker (in pixels)
 marker_separation = 20  # Separation between markers (in pixels)
 output_file = "marker_board.png"  # Output file name
 
 # Create the ArUco marker board
-create_aruco_marker_board(markers_X, markers_Y, marker_length, marker_separation, output_file)
+create_aruco_marker_board(markers_x, markers_y, marker_length, marker_separation, output_file)
--- marker_localization.py
+++ marker_localization.py
@@ -1,5 +1,7 @@
 import cv2
 import numpy as np
+import time
+from pymavlink import mavutil
 
 # Define the size of the marker in meters and the spacing between them
 marker_size = 0.027
@@ -13,65 +15,131 @@ aruco_params = cv2.aruco.DetectorParameters_create()
 camera_matrix = np.load('camera_matrix.npy')
 dist_coeffs = np.load('dist_coeffs.npy')
 
+# Video capture object
+cap = None
+
 # Create the aruco board object with 4x4 grid and 16 markers
-board = cv2.aruco.GridBoard_create(4, 4, marker_size, marker_spacing, aruco_dict)
+markers_x = 4
+markers_y = 4
+board = cv2.aruco.GridBoard_create(markers_x, markers_y, marker_size, marker_spacing, aruco_dict)
 
-# Open the video capture from source 0
-cap = cv2.VideoCapture(0)
+# Set the x y z coordinates
+x = 0
+y = 0
+z = 0
 
-# Loop until the user presses 'q' or the video ends
-while True:
-    # Read a frame from the video capture
-    ret, frame = cap.read()
-    if not ret:
-        break
-    
-    # Convert the frame to grayscale
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    
-    # Detect the aruco markers in the frame
-    corners, ids, rejected = cv2.aruco.detectMarkers(gray, aruco_dict, parameters=aruco_params)
-    
-    # If at least one marker is detected
-    if ids is not None:
-        # Draw the detected markers on the frame
-        frame = cv2.aruco.drawDetectedMarkers(frame, corners, ids)
-        
-        # Estimate the pose of the board relative to the camera
-        ret, rvec, tvec = cv2.aruco.estimatePoseBoard(corners, ids, board, camera_matrix, dist_coeffs, None, None, False)
-        
-        # If the pose estimation is successful
-        if ret > 0:
-            # Draw the axis of the board on the frame
-            frame = cv2.drawFrameAxes(frame, camera_matrix, dist_coeffs, rvec, tvec, marker_size)
+# Rate limiter set up
+frequency = 30
+period = 1 / frequency
+t0 = time.perf_counter()
+time_counter = t0
+target_time = t0
 
-        # Determine rvec and tvec from the board's frame of reference
-        R, jacobian = cv2.Rodrigues(rvec)
-        R = np.matrix(R).T
-        inv_tvec = np.dot(R, np.matrix(-tvec))
-        inv_rvec, jacobian = cv2.Rodrigues(R)
+# Connection information
+connection_address = '/dev/ttyTHS1'
+baud_rate = 57600
 
-        # The obtained rvec and tvec are from the camera's frame of reference to the frame of reference of the origin of the marker board
-        rvec = inv_rvec
-        tvec = inv_tvec
+# Create connection
+master = mavutil.mavlink_connection(connection_address, baud=baud_rate)
+master.wait_heartbeat()
+print('Hearbeat Received!')
 
-        # Get the XYZ coordinates from the array
-        x = tvec[0, 0]
-        y = tvec[1, 0]
-        z = tvec[2, 0]
+def main():
+    while True:
+        # Open the video capture from source 0
+        if cap is None or not cap.isOpened():
+            try:
+                cap = cv2.VideoCapture(0)
+                if cap.isOpened():
+                    print('Video capture was opened successfully.')
+                else:
+                    print('Failed to open video capture. Retrying in 1 second...')
+                    time.sleep(1) # Wait for 1 second before retrying
 
-        print('x: ', x, 'y: ', y, 'z: ', z)
-    
-    # Show the frame in a window
-    cv2.imshow('Aruco Marker Board', frame)
-    
-    # Wait for a key press for 1 ms
-    key = cv2.waitKey(1)
-    
-    # If the key is 'q', break the loop
-    if key == ord('q'):
-        break
+            except cv2.error as e:
+                print('Error:', str(e))
+                print('Retrying in 1 second...')
+                time.sleep(1) # Wait for 1 second before retrying
 
-# Release the video capture and destroy all windows
-cap.release()
-cv2.destroyAllWindows()
+        else:
+            try:
+                # Read a frame from the video capture
+                ret, frame = cap.read()
+                if not ret:
+                    break
+                
+                # Convert the frame to grayscale
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                
+                # Detect the aruco markers in the frame
+                corners, ids, rejected = cv2.aruco.detectMarkers(gray, aruco_dict, parameters=aruco_params)
+                
+                # If at least one marker is detected
+                if ids is not None:
+                    # Draw the detected markers on the frame
+                    frame = cv2.aruco.drawDetectedMarkers(frame, corners, ids)
+                    
+                    # Estimate the pose of the board relative to the camera
+                    ret, rvec, tvec = cv2.aruco.estimatePoseBoard(corners, ids, board, camera_matrix, dist_coeffs, None, None, False)
+                    
+                    # If the pose estimation is successful
+                    if ret > 0:
+                        # Draw the axis of the board on the frame
+                        frame = cv2.drawFrameAxes(frame, camera_matrix, dist_coeffs, rvec, tvec, marker_size)
+
+                    # Determine rvec and tvec from the board's frame of reference
+                    R, jacobian = cv2.Rodrigues(rvec)
+                    R = np.matrix(R).T
+                    inv_tvec = np.dot(R, np.matrix(-tvec))
+                    inv_rvec, jacobian = cv2.Rodrigues(R)
+
+                    # The obtained rvec and tvec are from the camera's frame of reference to the frame of reference of the origin of the marker board
+                    rvec = inv_rvec
+                    tvec = inv_tvec
+
+                    # Get the XYZ coordinates from the array
+                    x = tvec[0, 0]
+                    y = tvec[1, 0]
+                    z = tvec[2, 0]
+
+                    print('X: ', x, 'Y: ', y, 'Z: ', z)
+
+                # Create a message with a vision position estimate
+                msg = master.mav.vision_position_estimate_encode(
+                    usec = int(time.time() * 1000000), # Current time in microseconds
+                    x = x, # X position in meters
+                    y = y, # Y position in meters
+                    z = z, # Z position in meters
+                    roll = 0, # Set roll angle to zero
+                    pitch = 0, # Set pitch angle to zero
+                    yaw = 0 # Set yaw angle to zero
+                )
+                
+                # Delay the code to run at set frequency
+                now = time.perf_counter()
+                target_time += period
+                if now < target_time:
+                    time.sleep(target_time - now)
+
+                # Send the message
+                master.mav.send(msg)
+                print(msg)
+                
+                # Show the frame in a window
+                cv2.imshow('Aruco Marker Board', frame)
+                
+                # Wait for a key press for 1 ms
+                key = cv2.waitKey(1)
+                
+                # If the key is 'q', break the loop
+                if key == ord('q'):
+                    break
+
+            finally:
+                # Release the video capture and destroy all windows
+                cap.release()
+                cv2.destroyAllWindows()
+
+# Run the main function
+if __name__ == '__main__':
+    main()
