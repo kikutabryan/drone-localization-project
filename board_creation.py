290ea7045ee66ed3a04fffd90b7f5f3104ce5a00
kikutabryan <kikutabryan@gmail.com>
2023-05-24 17:40:02 -0400

    changed tag size

--- board_creation.py
+++ board_creation.py
@@ -19,10 +19,11 @@ def create_aruco_marker_board(markers_y, markers_x, marker_length, marker_separa
     print(f"Marker board image saved as '{output_file}'")
 
 # Define the parameters for the marker board
-markers_x = 4  # Number of markers along the X-axis
-markers_y = 4  # Number of markers along the Y-axis
-marker_length = 100  # Length of each marker (in pixels)
-marker_separation = 20  # Separation between markers (in pixels)
+# 10 mm = 37.7953 PX
+markers_x = 2  # Number of markers along the X-axis
+markers_y = 2  # Number of markers along the Y-axis
+marker_length = 189  # Length of each marker (in pixels)
+marker_separation = 38  # Separation between markers (in pixels)
 output_file = "marker_board.png"  # Output file name
 
 # Create the ArUco marker board
