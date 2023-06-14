5344cf672bb02bf505e6ddfc61a31e6f4884aba0
kikutabryan <kikutabryan@gmail.com>
2023-06-14 14:01:52 -0400

    added checks for code

--- arm.py
+++ arm.py
@@ -12,12 +12,27 @@ master = mavutil.mavlink_connection(connection_string, baud=baud_rate)
 master.wait_heartbeat()
 print("Drone connected!")
 
+# Wait some time
 time.sleep(2)
 
 # Arm the drone
 master.arducopter_arm()
 
+# Check if the drone is armed
+if master.target_system == 1 and master.target_component == 1:
+    print("Drone is armed!")
+else:
+    print("Failed to arm the drone.")
+    exit()
+
+# Wait 3 seconds before disarming the drone
 time.sleep(3)
 
 # Disarm the drone
 master.arducopter_disarm()
+
+# Check if the drone is disarmed
+if not master.target_system == 1 or not master.target_component == 1:
+    print("Drone is disarmed!")
+else:
+    print("Failed to disarm the drone.")
